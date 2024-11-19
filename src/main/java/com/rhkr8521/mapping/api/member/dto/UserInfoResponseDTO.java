f81261f64ab75fd19af1e79a1d3905b069ff3007
rhkr8521 <rhkr8521@gmail.com>
2024-11-19 08:28:05 +0000

    [FIX] 사용자 정보 조회 반환 필드 수정

--- src/main/java/com/rhkr8521/mapping/api/member/dto/UserInfoResponseDTO.java
+++ src/main/java/com/rhkr8521/mapping/api/member/dto/UserInfoResponseDTO.java
@@ -7,16 +7,14 @@ import lombok.Getter;
 
 @Getter
 public class UserInfoResponseDTO {
-    private final Long id;
     private final String nickname;
-    private final String imageUrl;
+    private final String profileImage;
     private final Role role;
     private final String socialId;
 
     public UserInfoResponseDTO(Member member){
-        this.id = member.getId();
         this.nickname = member.getNickname();
-        this.imageUrl = member.getImageUrl();
+        this.profileImage = member.getImageUrl();
         this.socialId = member.getSocialId();
         this.role = member.getRole();
     }
